0dab98ca24322516d4d381b97fdd0cfd54826a3c
dArk10R4 <goylerov@gmail.com>
2023-08-09 23:17:48 +0400

    m

---
+++ core/storage/framework/views/a3b0a8373f3664deeb9beb90d94170b42f52c9de.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        <?php echo e(@$general->copyright); ?>
+
+    </div>
+
+</footer>
+<?php /**PATH C:\xampp\htdocs\hyip-v100\Scripts\core\resources\views/backend/layout/footer.blade.php ENDPATH**/ ?>
